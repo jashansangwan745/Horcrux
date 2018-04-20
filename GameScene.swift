de58f6f7affe944fa420fa9285cb385e1d5b693e
jashansangwan745 <38533826+jashansangwan745@users.noreply.github.com>
2018-04-20 17:45:39 +1000

    WEEk 5
    Movement are being redeveloped of the player with additional features.

--- GameScene.swift
+++ GameScene.swift
@@ -1,6 +1,6 @@
 //
 //  GameScene.swift
-//  Horcrux
+//  HorcruxMain
 //
 //  Created by JASHAN SANGWAN on 20/04/2018.
 //  Copyright © 2018 JASHAN SANGWAN. All rights reserved.
@@ -13,53 +13,61 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    
+    var Character:SKSpriteNode = SKSpriteNode()
+    var Speeed:TimeInterval= 0.6
+    //Increases the speed of the player
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        self.physicsWorld.gravity = CGVector(dx: 1, dy: 0)
+        // provides a little wind to the character
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        if let Player:SKSpriteNode = self.childNode(withName: "Main_Player") as? SKSpriteNode{
+            Character = Player
+            Character.physicsBody?.isDynamic = false
+            self.Character.physicsBody?.affectedByGravity = false
+            //if set to true wont be affected by gravity
         }
+
     }
-    
+    func moveDown() {
+        let walkAnimation:SKAction = SKAction(named: "WalkUp")!
+        
+        let wait:SKAction = SKAction.wait(forDuration: Speeed //to set some frames of duration
+        
+        let moveAction:SKAction = SKAction.moveBy(x: 0, y: -50, duration: Speeed)
+        
+        let group:SKAction = SKAction.group([walkAnimation, moveAction])
+        
+        let end:SKAction = SKAction.run {
+            
+            
+            print("Ended")
+        }
+        
+        let seq:SKAction = SKAction.sequence([wait, group, end])
+        
+        Character.run(seq)
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+        
+        if ( pos.y > 0){
+            
+            //upper half touch
+            
         }
+        else{
+        //goes down half
+            moveDown()
+        
     }
-    
+    }
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+       
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+       
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
